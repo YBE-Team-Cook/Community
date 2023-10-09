40babfbbee19a28702091c6baaeaf8bd086f9695
Ok JaeOok <okjaeook98@gmail.com>
2023-10-09 18:02:19 +0900

    [Feat] Infra 컨테이너 구축
    컨테이너 환경 구축(Dcoker, docker-compose)
    Springboot 운영 환경 별 Profile 분리
    개발환경 Mysql 초기 세팅

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+FROM gradle:8.2.1-jdk17-alpine AS builder
+
+WORKDIR /build
+COPY . /build
+
+ARG EnvironmentVariable
+ARG RAILWAY_ENVIRONMENT
+ENV RAILWAY_ENVIRONMENT=$RAILWAY_ENVIRONMENT
+
+# 개행문자 오류 해결 [unix와 window 시스템 차이]
+RUN sed -i 's/\r$//' gradlew
+
+RUN gradle build -x test --parallel
+
+FROM openjdk:17-slim
+
+ARG MYSQLHOST
+ARG MYSQLDATABASE
+ARG MYSQLPORT
+ARG MYSQLUSER
+ARG MYSQLPASSWORD
+ARG APP_PROFILE
+
+ENV MYSQLHOST ${MYSQLHOST}
+ENV MYSQLDATABASE ${MYSQLDATABASE}
+ENV MYSQLPORT ${MYSQLPORT}
+ENV MYSQLUSER ${MYSQLUSER}
+ENV MYSQLPASSWORD ${MYSQLPASSWORD}
+ENV APP_PROFILE ${APP_PROFILE}
+
+WORKDIR /app
+COPY --from=builder /**/build/libs/*.jar app.jar
+
+MAINTAINER ybe.teamcook7@gmail.com
+
+EXPOSE 8080
+
+USER root
+
+# 환경 변수 설정
+ENV JAVA_TOOL_OPTIONS="-Dspring.profiles.active=prod"
+
+# 애플리케이션 실행
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ database/init.sql
@@ -0,0 +1,31 @@
+alter user 'root'@'localhost' identified with caching_sha2_password by 'root';
+flush privileges;
+
+create database if not exists community;
+
+use community;
+
+create table if not exists account
+(
+    account_id  int auto_increment comment '계정 ID' primary key,
+    email       varchar(45)                        not null comment '이메일',
+    name        varchar(45)                        not null comment '이름',
+    password    varchar(255)                       not null comment '비밀번호',
+    created_at  datetime default CURRENT_TIMESTAMP not null comment '생성일',
+    modified_at datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정일'
+)
+    comment '계정';
+
+create table if not exists board
+(
+    board_id    int auto_increment comment '게시판 ID' primary key,
+    title       varchar(255)                       not null comment '제목',
+    content     mediumtext                         not null comment '내용',
+    created_at  datetime default CURRENT_TIMESTAMP not null comment '생성일',
+    modified_at datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정일',
+    account_id  int                                not null comment '계정 ID',
+    constraint board_account_account_id_fk
+        foreign key (account_id) references account (account_id)
+            on update cascade on delete cascade
+)
+    comment '게시판';
