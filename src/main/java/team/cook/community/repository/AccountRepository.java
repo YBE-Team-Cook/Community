8073459c46f670102e778e17a47c720e874b910f
KwonMinwoo <qwerty029369@naver.com>
2023-10-19 13:26:48 +0900

    [Feat] Account Repository 작성 #38
    Account JPA Repository 추가

--- src/main/java/team/cook/community/repository/AccountRepository.java
+++ src/main/java/team/cook/community/repository/AccountRepository.java
@@ -1,4 +1,8 @@
 package team.cook.community.repository;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.cook.community.domain.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
 }
--- src/main/java/team/cook/community/repository/impl/AccountRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package team.cook.community.repository.impl;
-
-import org.springframework.stereotype.Repository;
-import team.cook.community.repository.AccountRepository;
-
-@Repository
-public class AccountRepositoryImpl implements AccountRepository {
-}
